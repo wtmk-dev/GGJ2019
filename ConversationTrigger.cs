491e296ccfd46f0de97885dd5f7a9a4183747970
WTMK-PC\WTMK <wtmk.dev@icloud.com>
2019-01-25 22:04:35 -0800

    Player movement conversation display

---
+++ ConversationTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationTrigger : MonoBehaviour {
+
+    public Action<int> OnConversationDisplay;
+    public Action<int> OnConversationHide;
+    [SerializeField]
+    private int ID;
+
+    void OnTriggerEnter2D( Collider2D other ) {
+        Debug.Log( other );
+        if( other.gameObject.tag == "Player" ){
+            if( OnConversationDisplay != null ){
+                OnConversationDisplay( ID );
+            }
+        }
+    }
+
+    void OnTriggerExit2D( Collider2D other ) {
+        Debug.Log( other );
+        if( other.gameObject.tag == "Player" ){
+            if( OnConversationHide != null ){
+                OnConversationHide( ID );
+            }
+        }
+    }
+}
