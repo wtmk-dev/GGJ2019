b3979c1991b15b63b6d5d6f6200ec00f2d169c42
WTMK-PC\WTMK <wtmk.dev@icloud.com>
2019-01-27 14:53:35 -0800

    screen fading
    end game screen fade

--- Scripts/GameScreenController.cs
+++ Scripts/GameScreenController.cs
@@ -9,11 +9,14 @@ public class GameScreenController : MonoBehaviour {
 
     public static Action<Screen> OnScreenComplete;
     [SerializeField]
-    private GameObject titleScreen, gameScreen, creditsScreen;
+    private GameObject titleScreen, gameScreen, creditsScreen, fadeScreen;
     [SerializeField]
     private TextMeshProUGUI endingText;
-
+    [SerializeField]
+    private Sprite endGameSprite;
+    private IEnumerator screenFade;
     private string endingString;
+    private Image startImg;
 
     void OnEnable(){
         GameController.OnScreenChange += ScreenChanged;
@@ -21,6 +24,10 @@ public class GameScreenController : MonoBehaviour {
 
     void OnDisable(){
         GameController.OnScreenChange -= ScreenChanged;
+    }
+
+    void Awake(){
+        startImg = fadeScreen.GetComponent<Image>();
     }
 
     public void StartGame(){
@@ -33,6 +40,14 @@ public class GameScreenController : MonoBehaviour {
 
     public void WakeUp(){
         gameScreen.SetActive( false );
+
+        if( screenFade != null ){
+            StopCoroutine( screenFade );
+        }
+        endingString = "Slept.";
+        fadeScreen.SetActive( true ); 
+        screenFade = FadeImageRoutine( true );
+        StartCoroutine( screenFade );
     }
 
     public void UpdateAccomplishments( Dictionary<int,bool> accompMap ){
@@ -73,17 +88,74 @@ public class GameScreenController : MonoBehaviour {
     }
 
     private void ScreenChanged( Screen currentScreen ){
+        if( screenFade != null ){
+            StopCoroutine( screenFade );
+        }
+
         if( currentScreen == Screen.Title ){
+            fadeScreen.SetActive( false );
             titleScreen.SetActive( true );
         }
 
         if( currentScreen == Screen.Game ){
             gameScreen.SetActive( true );
+            fadeScreen.SetActive( true );
+            screenFade = FadeImageRoutine( false );
+            StartCoroutine( screenFade );
         }
 
         if( currentScreen == Screen.Credits ){
+            fadeScreen.SetActive( true );
+            startImg.sprite = endGameSprite;
+            screenFade = FadeImageRoutine( true );
+            StartCoroutine( screenFade );
             creditsScreen.SetActive( true );
             endingText.text = endingString;
         }
+
     }
+
+    private IEnumerator FadeImageRoutine(bool isWake ) {
+        if( isWake ){
+            for ( float i = 1; i >= 0; i -= Time.deltaTime ) {
+                // set color with i as alpha
+                startImg.color = new Color(1, 1, 1, i);
+                yield return new WaitForEndOfFrame();
+            }
+        } else {
+             // loop over 1 second backwards
+            for ( float i = 1; i >= 0; i -= Time.deltaTime ) {
+                // set color with i as alpha
+                startImg.color = new Color(1, 1, 1, i);
+                yield return new WaitForEndOfFrame();
+            }
+
+            for ( float i = 0; i <= 1; i += Time.deltaTime ) {
+                // set color with i as alpha
+                startImg.color = new Color(1, 1, 1, i);
+                yield return new WaitForEndOfFrame();
+            }
+
+            for ( float i = 1; i >= 0; i -= Time.deltaTime ) {
+                // set color with i as alpha
+                startImg.color = new Color(1, 1, 1, i);
+                yield return new WaitForEndOfFrame();
+            }
+
+            for ( float i = 0; i <= 1; i += Time.deltaTime ) {
+                // set color with i as alpha
+                startImg.color = new Color(1, 1, 1, i);
+                yield return new WaitForEndOfFrame();
+            }
+
+            for ( float i = 1; i >= 0; i -= Time.deltaTime ) {
+                // set color with i as alpha
+                startImg.color = new Color(1, 1, 1, i);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    
+        fadeScreen.SetActive( false );
+       
+	}
 }
